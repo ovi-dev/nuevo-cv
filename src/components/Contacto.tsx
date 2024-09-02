a7a59dba6b998982796a7dfd0ed048a58870b0a1
adrian <adrian-perez@msn.com>
2024-09-02 17:24:11 +0200

    contactame

--- src/components/Contacto.tsx
+++ src/components/Contacto.tsx
@@ -1,7 +1,67 @@
 import React from 'react'
+import {fadeIn} from '../variables/vairables'
+import { motion } from 'framer-motion'
+
 
 export const Contacto = () => {
   return (
-    <div id='contacto' className='section'>Contacto</div>
+    <section id='contacto' className='py-16 lg:section'>
+      <div className=' container mx-auto'>
+        <div className=' flex flex-colo lg:flex-row'>
+          {/* Texto */}
+          <div className='flex-1'> 
+            <div>
+                <h4 className=' text-xl uppercase text-accent font-medium mb-2 
+                tracking-wide'>
+                  Ponte en contacto
+                  </h4>
+                  {/* tamano de letra text-[45px]  */}
+                  <h2 className='text-[45px] lg:text-[90px] leading-none mb-12'>Trabajemos <br/> Juntos</h2>
+
+            </div>  
+          </div>
+
+
+
+          {/* form */}
+          <div className='flex-1'>
+            <input 
+            type="text" 
+            name="" 
+            id=""
+            placeholder=' Tu Nombre' 
+            className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white
+             focus:border-accent transition-all'
+            />
+             <input 
+            type="text" 
+            name="" 
+            id=""
+            placeholder=' Tu email' 
+            className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white
+             focus:border-accent transition-all'
+            />
+            <textarea 
+            placeholder='Tu Mensaje'
+            className='bg-transparent border-b py-12 outline-none w-full placeholder:text-white
+             focus:border-accent transition-all resize-none mb-12'>
+             </textarea>
+             <button type="button"
+             className='rounded-xl bg-gradient-to-br from-[#6025F5] to-[#FF5555] px-5 py-3 text-base  text-white font-bold transition duration-200 hover:shadow-lg hover:shadow-[#6025F5]/50'
+             > Enviar Mensaje</button>
+
+          </div>
+
+
+
+
+        </div>
+      </div>
+      
+      
+      
+      
+      
+    </section>
   )
 }
--- src/components/Trabajos.tsx
+++ src/components/Trabajos.tsx
@@ -16,33 +16,35 @@ export const Trabajos = () => {
   return (
     <section id="trabajo" className="">
       <div className=" container mx-auto">
-        <div className="flex flex-col lg:flex-row gap-x-10">
-          <motion.div
+        <div className="flex flex-col lg:flex-row gap-x-12">
+          
+   <motion.div 
             variants={fadeIn("right", 0.2)}
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
             className=" flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0"
           >
-            {/* texto */}
+            {/* izquierda texto */}
             <div>
               <h2 className={`${inter.className} text-5xl text-accent mb-7`}>
                 My trabajo
               </h2>
-              <p className=" max-w-sm mb-16">
+              <p className=" max-w-sm mb-14">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                jlkjkljdslkjsd kjkj khj kjh kjh kjh kjh kjhas
+                jlkjkljdslkjsd kjkj khj kjh
                 
               </p>
               <button
-                className="rounded-xl bg-gradient-to-br from-[#6025F5] to-[#FF5555] px-5 py-3 text-base  text-white font-bold transition duration-200 hover:shadow-lg hover:shadow-[#6025F5]/50"
+                className="rounded-xl bg-gradient-to-br from-[#6025F5] to-[#FF5555] px-4 py-2 text-base  text-white font-bold transition duration-200 hover:shadow-lg hover:shadow-[#6025F5]/50"
                 type="button"
-              >
-          
-                Todos mis Proyectos
+              > Todos mis Proyectos
               </button>
             </div>
-            {/* imagenes */}
+
+
+
+            {/* imagenes div de primera imagen*/}
             <div
               className="group relative overflow-hidden border-2
              border-white/50 rounded-xl"
@@ -50,11 +52,13 @@ export const Trabajos = () => {
               {/* cubrir */}
               <div
                 className="group-hover:bg-black/70 w-full h-full absolute 
-              z-40 transition-all duration-300"
+              z-39 transition-all duration-300"
               ></div>
+
               {/* imagen */}
               <Image
                 className=" group-hover:scale-125 transition-all duration-500"
+                width={600}
                 src={Img1}
                 alt="primerta imagen"
               />
@@ -71,19 +75,20 @@ export const Trabajos = () => {
               >
                 <span className="text-3xl text-white">Proyectos</span>
               </div>
-            </div>
-          </motion.div>
+            </div> 
+  </motion.div> {/* // cierre imagen 1 */}
+
 
           {/* las otras dos Imagenes */}
-          <motion.div
+  <motion.div 
             variants={fadeIn("left", 0.2)}
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
-            className=" flex-1 flex flex-col gap-y-10"
+            className=" flex-1 flex flex-col gap-y-2"
           >
-            {/* Imagen 2 */}
-            <div className="group relative overflow-hidden border-2
+      {/* Imagen 2 */}
+      <div className="group relative overflow-hidden border-2
              border-white/50 rounded-xl"
             >
               {/* cubrir */}
@@ -91,10 +96,13 @@ export const Trabajos = () => {
               z-40 transition-all duration-300"
               ></div>
               {/* imagen */}
+              
               <Image
+              width={600}
                 className=" group-hover:scale-125 transition-all duration-500"
                 src={Img2}
-                alt="primerta imagen"
+                alt="segunda imagen"
+
               />
               {/* title */}
               <div className=" absolute -bottom-full left-12
@@ -109,9 +117,10 @@ export const Trabajos = () => {
               >
                 <span className="text-3xl text-white">Proyectos</span>
               </div>
-            </div>
-            {/* Imagen 3 */}
-            <div className="group relative overflow-hidden border-2
+        </div>
+
+        {/* Imagen 3 */}
+        <div className="group relative overflow-hidden border-2
              border-white/50 rounded-xl"
             >
               {/* cubrir */}
@@ -119,10 +128,12 @@ export const Trabajos = () => {
               z-40 transition-all duration-300"
               ></div>
               {/* imagen */}
-              <Image
+              
+              <Image 
+                width={600}
                 className=" group-hover:scale-125 transition-all duration-500"
                 src={Img3}
-                alt="primerta imagen"
+                alt="tercera imagen"
               />
               {/* title */}
               <div className=" absolute -bottom-full left-12
@@ -137,8 +148,9 @@ export const Trabajos = () => {
               >
                 <span className="text-3xl text-white">Proyectos</span>
               </div>
-            </div>
-          </motion.div>
+        </div>
+
+  </motion.div>
         </div>
       </div>
     </section>
